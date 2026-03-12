4434a76bfbefc0af3f00d8e6f89916bebbfd464f
Supertiger <saawalkhn@googlemail.com>
2026-03-12 08:29:53 +0000

    include sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import nested from 'postcss-nested';
 
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   css: {
     postcss: {
       plugins: [
